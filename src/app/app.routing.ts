ee9a14efff332e8be092f731197ffa0e11bb38c3
Antoine Coskun <antoine.csk@gmail.com>
2018-06-07 22:28:52 +0200

    #10 Added breadcrumb

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { CompanyComponent, LaunchesComponent, LaunchComponent } from './pages';
 
 const appRoutes: Routes = [
-  { path: 'company', component: CompanyComponent, data: { breadcrumb: 'company'}},
-  { path: 'launches/:id', component: LaunchComponent, data: { breadcrumb: 'company'}},
-  { path: 'launches', component: LaunchesComponent, data: { breadcrumb: 'company'}},
+  { path: 'company', component: CompanyComponent, data: { breadcrumbs: 'Company Informations'}},
+  { path: 'launches/:id', component: LaunchComponent, data: { breadcrumbs: 'Détails d\'une mission'}},
+  { path: 'launches', component: LaunchesComponent, data: { breadcrumbs: 'Missions'}},
 ];
 
 export const AppRouting = RouterModule.forRoot(appRoutes);
